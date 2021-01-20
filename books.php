a07ed0e723ee943f4e260c66737ada1e63099440
jaythan hew <jaythanhew@mail.weber.edu>
2021-01-19 18:53:46 -0700

    Assignment 2 Final

--- books.php
+++ books.php
@@ -1,4 +1,4 @@
 <?php
-header("Access-Control-Allow-Origin: *")
+header("Access-Control-Allow-Origin: *");
 ?>
 {books-read: [{"Title": "Percy Jackson and the Lightning Thief", "Author": "Rick Riordan"},{"Title": "The Gunslinger", "Author": "Stephen King"}]}
