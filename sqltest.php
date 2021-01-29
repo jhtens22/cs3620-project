012f662409199de89d459c2a2076900d6f80db29
jaythan hew <jaythanhew@mail.weber.edu>
2021-01-28 18:18:09 -0700

    session

--- sqltest.php
+++ sqltest.php
@@ -1,7 +1,11 @@
 <?php
+
+session_start();
+
 $servername = "cs3620-jaythan.mysql.database.azure.com";
-$username = $_ENV['SQLUSER'];
-$password = $_ENV['SQLPW'];
+
+$username = (isset($_SESSION["SQLUSER"]) ? $_SESSION["SQLUSER"] : $_ENV['SQLUSER']);
+$password = (isset($_SESSION["SQLPW"]) ? $_SESSION["SQLPW"] : $_ENV['SQLPW']);
 $dbname = "cs3620-project";
 
 // Create connection
