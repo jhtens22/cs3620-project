c7be655dc1854f0b92de36c2b6a461ea8feb0154
jaythan hew <jaythanhew@mail.weber.edu>
2021-01-28 17:42:32 -0700

    Assignment 3

--- sqltest.php
+++ sqltest.php
@@ -6,9 +6,6 @@ $dbname = "cs3620-project";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
-$con=mysqli_init(); 
-mysqli_ssl_set($con, NULL, NULL, "/var/www/html/BaltimoreCyberTrustRoot.crt.pem", NULL, NULL); 
-mysqli_real_connect($con, "cs3620-jaythan.mysql.database.azure.com", "jaythanhew@cs3620-jaythan", "TheseDays17", "cs3620-project", 3306);
 
 // Check connection
 if ($conn->connect_error) {
