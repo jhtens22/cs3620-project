885522d251a1ab2887507bbc922b882b3915f3d8
jaythan hew <jaythanhew@mail.weber.edu>
2021-02-04 18:46:05 -0700

    user

---
+++ user/user.php
@@ -0,0 +1,12 @@
+<?php
+
+Class User{
+    public function getUser($user_id){
+
+    }
+    public function getUsername(){
+        return "Gabe";
+    }
+}
+
+?>
---
+++ user_view.php
@@ -0,0 +1,16 @@
+<?php 
+    require_once('./header.php');
+?>
+
+<?php 
+    require_once('./user/user.php');
+
+    $user = new user();
+    $user->getUser(1);
+
+    echo $user->getUsername();
+?>
+
+<?php 
+    require_once('./footer.php');
+?>
