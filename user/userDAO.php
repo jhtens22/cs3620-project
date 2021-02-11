8790eb33920d4dd2d182e869fe047928fb8b9e63
jaythan hew <jaythanhew@mail.weber.edu>
2021-02-11 16:03:27 -0700

    oop crud

--- user/userDAO.php
+++ user/userDAO.php
@@ -1,9 +1,10 @@
 <?php
+ini_set('display_errors',1);
+ini_set('display_startup_errors',1);
+error_reporting(E_ALL);
     class UserDAO {
         function getUser($user){
-          ini_set('display_errors',1);
-          ini_set('display_startup_errors',1);
-          error_reporting(E_ALL);
+
           require_once('./utilities/connection.php');
           
           $sql = "SELECT first_name, last_name, username, user_id FROM user WHERE user_id =" . $user->getUserId();
